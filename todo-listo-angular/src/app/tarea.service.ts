a9fcb50aefda541792600e2ffcfd1582c0925d53
Unknown <fabian.vergara.l@mail.pucv.cl>
2018-12-16 16:53:08 -1200

    fronend de ayuda

--- todo-listo-angular/src/app/tarea.service.ts
+++ todo-listo-angular/src/app/tarea.service.ts
@@ -1,30 +1,35 @@
 import { Injectable } from '@angular/core';
 import { Tarea } from './tarea';
 import { Observable, of, empty } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TareaService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
-  tareas: Array<Tarea> = [
-    new Tarea(1, 'Comprar leche', 'Y pañales para el bebe')
-    , new Tarea(2, 'Hacer Taller Angular', 'Falta empezar...')
-    , new Tarea(3, 'Preparar papers', 'Por fin')
-  ];
-
-crearTarea(t: Tarea): Observable<any> {
-    // Obtener maximo id en this.tareas e incrementar en 1 para el nuevo id
-    const newId = Math.max.apply(null, this.tareas.map(x => x.id)) + 1;
-    // Insertar en el 'backend' la nueva tarea con el id generado y sus atributos
-    this.tareas.push(new Tarea(newId, t.titulo, t.descripcion));
-    // Se retorna un observable vacío solamente para seguir usando observables
-    return empty();
+  crearTarea(t: Tarea): Observable<any> {
+    return this.http.post('http://localhost:8000/tareas/', {
+      'titulo': t.titulo,
+      'descripcion': t.descripcion,
+      'estado': t.estado,
+    })
   }
 
-  getTareas(): Observable<any> {
-    return of(this.tareas);
+  actualizarTarea(t: Tarea): Observable<any> {
+    return this.http.put(`http://localhost:8000/tareas/${t.id}/`, {
+      'titulo': t.titulo,
+      'descripcion': t.descripcion,
+      'estado': t.estado,
+    })
+  }
+
+  getTareas(user_token): Observable<any> {
+    console.log(`Tarea service user_token: ${user_token}`);
+    return this.http.get('http://localhost:8000/tareas/', {
+      headers: {'Authorization': `Token ${user_token}`}
+    });
   }
 }
